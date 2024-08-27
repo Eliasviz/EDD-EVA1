0bea38c190e592b2f3316f83972a622213611233
Eliasviz <158387644+Eliasviz@users.noreply.github.com>
2024-08-26 22:11:59 -0600

    Add files via upload

---
+++ EVA1_3_HEAP/src/main/java/com/mycompany/eva1_3_heap/EVA1_3_HEAP.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_3_heap;
+
+/**
+ *
+ * @author Elias
+ */
+public class EVA1_3_HEAP {
+
+    public static void main(String[] args) {
+        int i = 5;
+        Prueba obj = new Prueba();
+        Prueba obj2 = new Prueba();
+        System.out.println(i);
+        System.out.println(obj);
+        System.out.println(obj2);
+    }
+}
+class Prueba{
+    
+}
