d9daa9b9e01f6291cf481fe727044abced1cf69e
Eliasviz <158387644+Eliasviz@users.noreply.github.com>
2024-08-26 22:11:47 -0600

    Add files via upload

---
+++ EVA1_2_STAKOVERFLOW/src/main/java/com/mycompany/eva1_2_stakoverflow/EVA1_2_STAKOVERFLOW.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2_stakoverflow;
+
+/**
+ *
+ * @author Elias
+ */
+public class EVA1_2_STAKOVERFLOW {
+
+    public static void main(String[] args) {
+        A();
+    }
+    public static void A(){
+        B();
+    }
+    public static void B(){
+        A();
+    }
+}
