cff4d89f302e8bdce310065c23d548dc769cc69d
Eliasviz <158387644+Eliasviz@users.noreply.github.com>
2024-08-28 21:46:38 -0600

    Add files via upload

---
+++ EVA1_4_STACK_HEAP/src/eva1_4_stack_heap/EVA1_4_STACK_HEAP.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_stack_heap;
+
+/**
+ *
+ * @author Elias
+ */
+public class EVA1_4_STACK_HEAP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Prueba prueba = new Prueba();
+        System.out.println("Main: " + prueba);
+        foo(prueba);
+    }
+    public static void foo(Prueba Objeto){
+        System.out.println("foo: " + Objeto);
+    }
+}
+class Prueba{
+    
+}
