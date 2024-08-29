2e523f54e7496954db4e454117cb3d5b38fced1c
Eliasviz <158387644+Eliasviz@users.noreply.github.com>
2024-08-28 21:46:55 -0600

    Add files via upload

---
+++ EVAL1_6_ARREGLOS/src/eval1_6_arreglos/EVAL1_6_ARREGLOS.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eval1_6_arreglos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVAL1_6_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] arreglo = new int[10];
+        System.out.println(arreglo);
+        arreglo[0] = 100;
+        arreglo[9] = 1000;
+        System.out.println("Arreglo[0] = " + arreglo[0]);
+        System.out.println("Arreglo[9] = " + arreglo[9]);
+    }
+    
+}
